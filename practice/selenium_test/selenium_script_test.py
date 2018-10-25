2fbb5dd6921ba674c21b424e30847a02b79907b4
Ethan Lin <supra5588@gmail.com>
2018-10-25 14:25:09 +0800

    selenium module 範例
    爬蟲網頁及工具使用

--- practice/selenium_test/selenium_script_test.py
+++ practice/selenium_test/selenium_script_test.py
@@ -4,11 +4,12 @@ slenium
 """
 
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 import time
 
 driver = webdriver.Chrome('/Users/ethanlin/PycharmProjects/Hello/chromedriver')
+driver.get('https://www.google.com/')
 driver.fullscreen_window()  # if you use mac must add this code
-driver.get('https://www.google.com.tw')
 
 # """find_element_by_id"""
 # # driver.find_element_by_id("cellphone").send_keys('0965109929')
@@ -36,8 +37,43 @@ driver.get('https://www.google.com.tw')
 # """find_element_by_xpath"""
 # driver.find_element_by_xpath("//a[contains(text(),'Gmail')]").click()  # Xml type use ChroPath to find it
 
-"""find_element_by_css_selector"""
-driver.find_element_by_css_selector("#lst-ib").send_keys('hello world\n')
-driver.back()
-time.sleep(3)
+# """find_element_by_css_selector"""
+# driver.find_element_by_css_selector("#lst-ib").send_keys('hello world\n')
+# driver.back()
+# time.sleep(3)
+
+# driver.maximize_window()  # fullscreen windows it's use on windoes
+# driver.minimize_window()    # Mac 執行會報錯 windows 正常
+
+# time.sleep(2)
+# titleName = driver.title
+# print(titleName)
+#
+# currentUrl = driver.current_url
+# print(currentUrl)
+#
+# driver.get('https://www.w3schools.com/html/default.asp')    #會覆蓋先前網址
+# time.sleep(2)
+# driver.back()
+# time.sleep(2)
+# driver.forward()
+# urlSource = driver.page_source
+# print(urlSource)
+
+# selectElements = driver.find_element_by_id('month')
+# months = Select(selectElements)
+# months.select_by_visible_text('December')
+#
+# countriesElement = driver.find_element_by_id('country')
+# countries = Select(countriesElement)
+# countries.select_by_visible_text('Taiwan')
+#
+# driver.find_element_by_xpath("//form[@id='cf']//input[@value='View Calendar']").click()
+#
+# time.sleep(5)
+
+driver.find_element_by_id('lst-ib').send_keys("hello")
+
+driver.save_screenshot('截圖.jpg')  # 未指定路徑會存在當前資料夾下
+
 driver.quit()  # Close browser
