f5f92ce06364ec96b55ef9efad14d047fbb8acaf
Ethan Lin <supra5588@gmail.com>
2018-12-03 17:12:15 +0800

    Practice

---
+++ practice/input_practice.py
@@ -0,0 +1,10 @@
+# a_input = input("Place give me a number: ")     #It will return string
+a_input = int(input("Pleace give a number"))      #chang type to int
+if a_input == 1:
+    print("hello")
+elif a_input == 2:
+    print("test 2")
+elif a_input == 3:
+    print("this test 3")
+else:
+    print(("this is last test"))
